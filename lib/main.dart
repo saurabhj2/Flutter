75fe14ba74e4ffd61a797ac25cfc2c336e9e9fa6
saurabhj2 <jaiswalsaurabh888@gmail.com>
2019-05-29 11:14:24 +0530

    Testing 13

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
 import "package:flutter/material.dart";
-
-void main() {
-  runApp(new testingPerpose());
-}
-
-class testingPerpose extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "My Flutter Apps",
-      home: Scaffold(
-        appBar: AppBar(title: Text("My Apps"),),
-        body: new Material(
-        color: Colors.cyan,
-        child: Center(
-        child: Text("Hello World",textDirection: TextDirection.ltr,
-        style: TextStyle(color:Colors.white,fontSize:40.0)
-        )
-        )
-      )
-      
-      )
-      );
-  }
+import "./testing_perpose/TestingApps.dart";
+void main(){
+  runApp(new MaterialApp(
+   title: "Test",
+   color: Colors.blueGrey,
+   home: TestingApps()
+  )
+  );
 }
