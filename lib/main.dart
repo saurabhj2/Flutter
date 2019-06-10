9d800c37917d177e66fd7e99eec01770eb65812f
saurabhj2 <jaiswalsaurabh888@gmail.com>
2019-06-10 10:59:10 +0530

    he

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import "package:flutter/material.dart";
 import "./testing_perpose/TestingApps.dart";
-void main(){
-  runApp(new MaterialApp(
-   title: "Test",
-   color: Colors.blueGrey,
-   home: TestingApps()
-  )
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: "Test",
+      color: Colors.blueGrey,
+      home: TestingApps(),
+    ),
   );
 }
--- lib/testing_perpose/TestingApps.dart
+++ lib/testing_perpose/TestingApps.dart
@@ -1,24 +1,24 @@
 import "package:flutter/material.dart";
 
-class TestingApps extends StatelessWidget{
+class TestingApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return Center(
-        child: Container(
+    return Center(
+      child: Container(
         alignment: Alignment.center,
         color: Colors.blueAccent,
         //margin: EdgeInsets.only(right:15.0,left:15.0,top:15.0,bottom:40.0),
-        child: Text("Hello", textDirection: TextDirection.ltr,
-        style: TextStyle(decoration: TextDecoration.none,
-        fontSize: 15.1,
-        fontFamily: 'Raleway',
-        fontWeight: FontWeight.w700,
-        color: Colors.white
-      )
-      
-      )
-      )
-      );
+        child: Text(
+          "Raebareli",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 30.1,
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w700,
+              color: Colors.white),
+        ),
+      ),
+    );
   }
-
 }
